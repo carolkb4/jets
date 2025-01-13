47f8b964401e4aa7b27ddb81f7117fb95a3ce100
carolinas@knowbe4.com <carolinas@knowbe4.com>
2025-01-13 17:55:59 -0300

    adding conditional for include_body_parameters

--- lib/jets/controller/request/compat/params.rb
+++ lib/jets/controller/request/compat/params.rb
@@ -1,5 +1,5 @@
-require "action_dispatch/request/utils"
-require "rack"
+require 'action_dispatch/request/utils'
+require 'rack'
 
 module Jets::Controller::Request::Compat
   # Provides these 3 methods to make it Rails compatible:
@@ -27,31 +27,36 @@ module Jets::Controller::Request::Compat
     #   3. body parameters
     def parameters(include_path_params: true, include_body_params: true)
       params = {}
-      params = params.deep_merge(body_params) if body_params && !body_params.is_a?(Array)
+      if include_body_params
+        params = if request_parameters.is_a?(Array)
+                   params.deep_merge({ _json: request_parameters })
+                 else
+                   params.deep_merge(request_parameters)
+                 end
+      end
       p "params: #{params}"
       p "body_params: #{body_params}"
       params = params.deep_merge(unescape_recursively(query_parameters)) # always
       params = params.deep_merge(path_parameters) if include_path_params
       params = set_binary_encoding(params)
-      params = normalize_encode_params(params)
-      params
+      normalize_encode_params(params)
     end
     memoize :parameters
     alias params parameters
 
     def path_parameters
-      path_params = event["pathParameters"] || {}
+      path_params = event['pathParameters'] || {}
       path_params.merge!(path_parameters_defaults)
-      path_params = path_params.map { |k, path| [k, CGI.unescape(path.to_s)] }.to_h
+      path_params.map { |k, path| [k, CGI.unescape(path.to_s)] }.to_h
     end
 
     def path_parameters_defaults
       route = Jets::Router::Matcher.new.find_by_request(self)
       if route
-        controller = route.controller_name.delete_suffix("Controller").underscore
+        controller = route.controller_name.delete_suffix('Controller').underscore
         {
           controller: controller,
-          action: route.action_name,
+          action: route.action_name
         }.merge(route.resolved_defaults)
       else
         {}
@@ -61,8 +66,8 @@ module Jets::Controller::Request::Compat
     # Based on ActionDispatch::Request#GET https://bit.ly/48hvDwe
     # Override Rack's GET method to support indifferent access.
     def GET
-      fetch_header("action_dispatch.request.query_parameters") do |k|
-        params = event["queryStringParameters"] || {}
+      fetch_header('action_dispatch.request.query_parameters') do |k|
+        params = event['queryStringParameters'] || {}
         set_binary_encoding(params)
         set_header k, normalize_encode_params(params)
       end
@@ -72,7 +77,7 @@ module Jets::Controller::Request::Compat
     # Based on ActionDispatch::Request#POST https://bit.ly/48fxh1A
     # Override Rack's POST method to support indifferent access.
     def POST
-      fetch_header("action_dispatch.request.request_parameters") do
+      fetch_header('action_dispatch.request.request_parameters') do
         params = get_request_parameters
         self.request_parameters = normalize_encode_params(params)
       end
@@ -81,19 +86,20 @@ module Jets::Controller::Request::Compat
 
     def request_parameters=(params)
       raise if params.nil?
-      set_header("action_dispatch.request.request_parameters", params)
+
+      set_header('action_dispatch.request.request_parameters', params)
     end
 
     def get_request_parameters
-      body = event['isBase64Encoded'] ? base64_decode(event["body"]) : event["body"]
+      body = event['isBase64Encoded'] ? base64_decode(event['body']) : event['body']
       return {} if body.nil?
 
       parsed_json = parse_json(body)
       return parsed_json if parsed_json
 
-      if content_type.to_s.include?("application/x-www-form-urlencoded")
+      if content_type.to_s.include?('application/x-www-form-urlencoded')
         return ::Rack::Utils.parse_nested_query(body)
-      elsif content_type.to_s.include?("multipart/form-data")
+      elsif content_type.to_s.include?('multipart/form-data')
         return parse_multipart(body)
       end
 
@@ -103,24 +109,24 @@ module Jets::Controller::Request::Compat
 
     # jets specific
     def request_method_from_hidden_method_field
-      get_request_parameters["_method"].to_s.upcase if get_request_parameters["_method"]
+      get_request_parameters['_method'].to_s.upcase if get_request_parameters['_method']
     end
 
-  private
+    private
 
     def parse_multipart(body)
       boundary = ::Rack::Multipart::Parser.parse_boundary(content_type)
       options = multipart_options(body, boundary)
-      env = ::Rack::MockRequest.env_for("/", options)
+      env = ::Rack::MockRequest.env_for('/', options)
       ::Rack::Multipart.parse_multipart(env) # params Hash
     end
 
-    def multipart_options(data, boundary = "AaB03x")
+    def multipart_options(data, boundary = 'AaB03x')
       type = %(multipart/form-data; boundary=#{boundary})
       length = data.bytesize
 
-      { "CONTENT_TYPE" => type,
-        "CONTENT_LENGTH" => length.to_s,
+      { 'CONTENT_TYPE' => type,
+        'CONTENT_LENGTH' => length.to_s,
         :input => StringIO.new(data) }
     end
 
@@ -132,6 +138,7 @@ module Jets::Controller::Request::Compat
 
     def base64_decode(body)
       return nil if body.nil?
+
       Base64.decode64(body)
     end
 
